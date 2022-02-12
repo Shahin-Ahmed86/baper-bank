46254ba4a41dfa9816b34eddf16d84bad861fbd8
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-02-12 22:47:19 +0800

    first commit

---
+++ js/bank.js
@@ -0,0 +1,17 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+//    console.log(userEmail)
+    
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPasword = passwordField.value;
+    // console.log(userPasword)
+
+    if (userEmail == 'mohammadshahinkhan1992@gmail.com' && 
+       userPasword =='shahin5459'){
+        console.log('valid user');
+        window.location.href = 'banking.html';
+    }
+    
+})
